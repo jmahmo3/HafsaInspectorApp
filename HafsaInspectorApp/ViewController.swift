fbf63bd7208c316449a6b584c2541edfb7d4f681
Junaid Mahmood <jmahmo3@uic.edu>
2016-08-29 00:00:24 -0500

    Initial Commit

---
+++ HafsaInspectorApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HafsaInspectorApp
+//
+//  Created by Junaid Mahmood on 8/29/16.
+//  Copyright © 2016 Mahmood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
