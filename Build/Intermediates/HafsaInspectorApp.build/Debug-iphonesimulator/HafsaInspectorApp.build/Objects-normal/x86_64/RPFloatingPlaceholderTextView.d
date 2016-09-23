680ee7687283c30624dc4f1ffd9d5d4573df405d
Sameer Siddiqui <sameer.siddiqui@keylimetie.com>
2016-09-23 15:15:39 -0500

    move material files in proper folder

---
+++ Build/Intermediates/HafsaInspectorApp.build/Debug-iphonesimulator/HafsaInspectorApp.build/Objects-normal/x86_64/RPFloatingPlaceholderTextView.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/sameersiddiqui/Projects/HafsaInspectorApp/HafsaInspectorApp/Utilities/CustomControls/RPFloatingPlaceholderTextView.m \
+  /Users/sameersiddiqui/Projects/HafsaInspectorApp/HafsaInspectorApp/Utilities/CustomControls/RPFloatingPlaceholderTextView.h \
+  /Users/sameersiddiqui/Projects/HafsaInspectorApp/HafsaInspectorApp/Utilities/CustomControls/RPFloatingPlaceholderConstants.h
