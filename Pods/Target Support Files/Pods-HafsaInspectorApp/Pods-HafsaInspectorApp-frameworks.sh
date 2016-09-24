f3a76ad9eb6d5e6862a03bcf0a764f316d9600bc
Sameer Siddiqui <ssiddi28@gmail.com>
2016-09-24 18:48:26 -0500

    install pods for tesseract

--- Pods/Target Support Files/Pods-HafsaInspectorApp/Pods-HafsaInspectorApp-frameworks.sh
+++ Pods/Target Support Files/Pods-HafsaInspectorApp/Pods-HafsaInspectorApp-frameworks.sh
@@ -85,7 +85,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-HafsaInspectorApp/MBProgressHUD.framework"
+  install_framework "Pods-HafsaInspectorApp/TesseractOCR.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-HafsaInspectorApp/MBProgressHUD.framework"
+  install_framework "Pods-HafsaInspectorApp/TesseractOCR.framework"
 fi
